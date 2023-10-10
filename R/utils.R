182fb302b9035f219d067f41c48f8fc469c8a881
Camden Blatchly <camden.blatchly@ruralinnovation.us>
2023-10-10 10:19:52 -0600

    continuous mapping

--- R/utils.R
+++ R/utils.R
@@ -27,10 +27,29 @@ get_bb_co <- function(geo = FALSE) {
 }
 
 
-#' Gets tract-level broadband data-set
-get_bb_tr <- function() {
+#' Gets tract-level broadband dataset
+get_bb_tr <- function(geo = FALSE, st = "33") {
   
-  bb_tr <- readr::read_csv("data/bb_tr_22.csv")
+  bb_tr <- readr::read_csv(here::here("data/bb_tr_22.csv")) |>
+    mutate(
+      pct_100_20_loc = cnt_100_20 / cnt_total_locations,
+      pct_25_3_loc = cnt_25_3 / cnt_total_locations,
+      pct_fiber_loc = cnt_fiber_locations / cnt_total_locations
+    )
+  
+  if (geo == FALSE) {
+    return(bb_tr)
+  }
+  
+  tracts <- tigris::tracts(state = st, cb = TRUE, year = 2021) %>%
+    dplyr::filter(STATEFP <= "56") %>%
+    tigris::shift_geometry(position = "below")
+  
+  bb_tr <- dplyr::left_join(
+    bb_tr |> dplyr::filter(geoid_st == st),
+    tracts,
+    by = c("geoid_tr" = "GEOID")
+  )
   
   return(bb_tr)
   
@@ -49,28 +68,26 @@ formatCutLabels <- function(breaks) {
 }
 
 
-get_choropleth <- function(dta, breaks, chrt_labels) {
+get_choropleth <- function(dta, breaks, chrt_labels, outline_width = 0.1) {
   
   fig <- dta %>%
     mutate(
       fill_var = cut(
         pct_25_3_loc,
         breaks$brks,
-        labels = formatCutLabels(breaks$brks)
+        labels = formatCutLabels(breaks$brks),
+        include.lowest = TRUE
       )
     ) %>%
     sf::st_as_sf() %>%
     ggplot() +
-    geom_sf(aes(fill = fill_var)) +
-    scale_fill_cori(
-      palette = "ctg3gn",
-    ) +
-    labs(chrt_labels) +
-    theme_cori_map() +
+    geom_sf(aes(fill = fill_var), color = "#d0d2ce", linewidth = outline_width) +
+    scale_fill_brewer(na.value = "#d0d2ce") + 
+    # scale_color_brewer(na.value = "#d0d2ce") +
+    theme_cori_map()
+  
+  fig <- fig + do.call(ggplot2::labs, chrt_labels)
 
   return(fig)
-  
-  # save_plot(fig, here::here("export/choropleth_jenks_co.png"), chart_height = 8)
-  
   
 }
